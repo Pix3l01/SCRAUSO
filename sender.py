ca96fa4ae2caa1f4980339a29cedc1ac5cf31f55
NemoElGringo <rico.borra@gmail.com>
2021-06-05 23:58:00 +0200

    Bozza

--- sender.py
+++ sender.py
@@ -1,20 +1,24 @@
 import sqlite3
+from requests import Request
+import requests
+import json
 
 class senderInterface(object):
     def send(self):
         raise NotImplementedError()
 
 class forcADsender(senderInterface):
-    def __init__(self, db):
-        self.db=db
+    def __init__(self, db, token):
+        self.db = db
+        self.token = token
 
     def send(self):
         try:
-            db = sqlite3.connect(db)
+            db = sqlite3.connect(self.db)
             cursor = db.cursor()
 
             #query placeholder
-            query="SELECT flag FROM flags WHERE used=0"
+            query = "SELECT flag FROM submitter WHERE used=0"
             cursor.execute(query)
 
             flags = []
@@ -22,7 +26,15 @@ class forcADsender(senderInterface):
                 flags.append(cursor.fetchone())
 
         except Exception as e:
-            print("Si è sminchiato tutto")
+            print("Si è sminchiato tutto....")
 
         finally:
             db.close()
+
+        jason = json.dumps(flags)
+        resp = requests.put(url=self.url, data=jason, headers=self.headers)
+
+        stats(json.loads(resp))
+
+    def stats(resp):
+        #coming soon
