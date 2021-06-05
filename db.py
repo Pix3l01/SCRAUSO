f5717c2c73ab3ed1e9ebde8cfaf66a08ac1cac92
Alessandro Pizzorni <pizzorni.alessandro@gmail.com>
2021-06-06 00:48:19 +0200

    Created simple sqlite db manager with database init query exec

---
+++ db.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+class database:
+    def __init__(self, db_path: str):
+        self.path = db_path
+
+    def init_database(self):
+        con = sqlite3.connect(self.path)
+        cur = con.cursor()
+
+        # Create table
+        cur.execute('''CREATE TABLE IF NOT EXISTS 'Submitter' (
+            'Flag' VARCHAR(50) NOT NULL,
+            'Tick' INT NULL,
+            'Status' INT NULL DEFAULT 0,
+            'Exploit' VARCHAR(45) NOT NULL,
+            PRIMARY KEY ('Flag'))''')
+
+        # Save (commit) the changes
+        con.commit()
+
+        # Close connection
+        con.close()
+
+    def exec_query(self, query: str):
+        con = sqlite3.connect(self.path)
+        cur = con.cursor()
+        res = cur.execute(query)
+        con.commit()
+        con.close()
+        return res
