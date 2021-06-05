d662a6a29a87bdd2776659ebedaf19a5a3b35392
NemoElGringo <rico.borra@gmail.com>
2021-06-05 23:25:01 +0200

    Bozza

--- sender.py
+++ sender.py
@@ -1,1 +1,31 @@
-#code goes here
+import sqlite3
+
+class senderInterface(object):
+
+    def send(self):
+        raise NotImplementedError()
+
+class forcADsender(senderInterface):
+    def __init__(self, db):
+        self.db=db
+
+    def send(self):
+        try:
+            db = sqlite3.connect(db)
+            cursor = db.cursor()
+
+            #query placeholder
+            query="SELECT flag FROM flags WHERE used=0"
+            cursor.execute(query)
+
+            flags = []
+            for row in cursor:
+                flags.append(cursor.fetchone())
+
+        except Exception as e:
+            print("Si è sminchiato tutto")
+
+        finally:
+            db.close()
+
+        
