62ca6e7dd0ec9fc91186f069899db78024fe539a
Alessandro Pizzorni <pizzorni.alessandro@gmail.com>
2021-06-09 01:22:41 +0200

    Added more configurations option and actually implemented call to sender.py

--- main.py
+++ main.py
@@ -2,28 +2,39 @@ import receiver
 import db
 import tomli
 import sys
+from sender import sender
 
 
-def start_receiver(dbm: db.database):
+def start_receiver(dbm: db.database, send: sender, port: int):
     receiver.setDbm(dbm)
-    receiver.app.run()
+    receiver.setSender(send)
+    receiver.app.run(host='0.0.0.0', port=port)
 
 
 def load_config(path: str):
+    print(
+        'I\'m lazy so the program check for the existence of every parameter of the config even if not necessary. So \
+        if you have error add also useless dummy parameters')
     try:
-        with open(sys.argv[1], encoding="utf-8") as f:
-            config_dict = tomli.load(f)
+        with open(path, encoding="utf-8") as f:
+            config = tomli.load(f)
     except IOError:
         print("Io error")
         exit(1)
     except tomli.TOMLDecodeError:
         print("Can't load config")
         exit(2)
-    assert 'link' in config_dict, 'Parameter \'link\' missing in config'
-    assert 'db' in config_dict, 'Parameter \'db\' missing in config'
-    assert 'token' in config_dict, 'Parameter \'token\' missing in config'
+    assert 'general' in config, 'Missing block \'general\' in config'
+    assert 'sender' in config, 'Missing block \'sender\' in config'
+    assert 'db' in config['general'], 'Parameter \'db\' missing in config block [general]'
+    assert 'port' in config['general'], 'Parameter \'port\' missing in config block [general]'
+    assert 'link' in config['sender'], 'Parameter \'link\' missing in config block [sender]'
+    assert 'token' in config['sender'], 'Parameter \'token\' missing in config block [sender]'
+    assert 'ip' in config['sender'], 'Parameter \'ip\' missing in config block [sender]'
+    assert 'port' in config['sender'], 'Parameter \'port\' missing in config block [sender]'
+    assert 'sender' in config['sender'], 'Parameter \'sender\' missing in config block [sender]'
     print('done!')
-    return config_dict
+    return config
 
 
 if __name__ == '__main__':
@@ -31,10 +42,19 @@ if __name__ == '__main__':
         print('\nIt needs a config file as argument')
         exit(0)
     print('Loading configs', end=' ')
-    config_dict = load_config(sys.argv[0])
-    dbm = db.database('test.db')
+    config_dict = load_config(sys.argv[1])
+    senderino: sender
+    if config_dict['sender']['sender'] == 'forcADsender':
+        senderino = sender(config_dict['general']['db'], config_dict['sender']['token'], config_dict['sender']['link'])
+    elif config_dict['sender']['sender'] == 'ncsender':
+        senderino = sender(config_dict['general']['db'], config_dict['sender']['token'], config_dict['sender']['ip'],
+                           config_dict['sender']['port'])
+    else:
+        print('Sender method not defined')
+        exit(3)
+    dbm = db.database(config_dict['general']['db'])
     print('Initializing database', end=' ')
     dbm.init_database()
     print('done!')
     print('Starting receiver')
-    start_receiver(dbm)
+    start_receiver(dbm, senderino, config_dict['general']['port'])
